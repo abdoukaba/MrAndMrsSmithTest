6c6f364c0bdc4b06c79003dddbcf25fa583d743c
abdoukaba <abdoukaba@users.noreply.github.com>
2017-06-05 16:24:56 +0200

    Update Calculator.php

--- symfony-cli-calculator-master/Application/Cli/Calculator.php
+++ symfony-cli-calculator-master/Application/Cli/Calculator.php
@@ -14,13 +14,15 @@ class Calculator extends Application {
      * Calculator constructor.
      */
     public function __construct() {
-    	parent::__construct('Welcome to Umpirsky CLI Calculator', '1.0');
+    	parent::__construct('Welcome to  CLI Calculator', '1.0');
     	
     	$this->addCommands(array(
 			new Command\Add(),
 			new Command\Sub(),
 			new Command\Mul(),
-			new Command\Div()
+			new Command\Div(),
+			new Command\And(),
+			new Command\Or()
 		));
     }
 }
